25225bb03e2c5ffef0a49be2774a0ab4589c302a
Francisco Vicente A. Neto <vicenteneto.ti@gmail.com>
2021-06-16 07:28:32 -0300

    Formatting the current date in the head component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,11 @@
+import format from 'date-fns/format';
+import { ptBR } from 'date-fns/locale';
+import ptBr from 'date-fns/locale/pt-BR';
+
 import styles from './styles.module.scss';
 
 export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d, MMMM', {locale: ptBR})
     return (
         <header className={styles.headerContainer}>
         
@@ -8,7 +13,7 @@ export function Header() {
 
             <p>Para se manter programando atualizado.</p>
 
-            <span>Quin, 8 Junho</span>
+            <span>{currentDate}</span>
         </header>
     );
 }
